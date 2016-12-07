d2ea160a1f9c2dcd2ee49218f988f878a9110641
c_jmao <mao_jinlong@126.com>
2016-12-07 21:27:25 +0800

    Zuke project

---
+++ app/src/main/java/com/android/zuke/MainActivity.java
@@ -0,0 +1,67 @@
+package com.android.zuke;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.BmobObject;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.SaveListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Bmob.initialize(this, "5bad1a4f2fd3f4d332dfd2070257b86d");
+
+        //第二：自v3.4.7版本开始,设置BmobConfig,允许设置请求超时时间、文件分片上传时每片的大小、文件的过期时间(单位为秒)，
+        //BmobConfig config =new BmobConfig.Builder(this)
+        ////设置appkey
+        //.setApplicationId("5bad1a4f2fd3f4d332dfd2070257b86d")
+        ////请求超时时间（单位为秒）：默认15s
+        //.setConnectTimeout(30)
+        ////文件分片上传时每片的大小（单位字节），默认512*1024
+        //.setUploadBlockSize(1024*1024)
+        ////文件的过期时间(单位为秒)：默认1800s
+        //.setFileExpiration(2500)
+        //.build();
+        //Bmob.initialize(config);
+
+
+        Person p2 = new Person();
+        p2.setName("lucky");
+        p2.setAddress("北京海淀");
+        p2.save(new SaveListener<String>() {
+            @Override
+            public void done(String s, BmobException e) {
+                if(e==null){
+                    Log.d("zuke", "添加数据成功，返回objectId为："+ s);
+                }else{
+                    Log.d("zuke", "创建数据失败：" + e.getMessage());
+                }
+            }
+        });
+    }
+
+    public class Person extends BmobObject {
+        private String name;
+        private String address;
+
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public String getAddress() {
+            return address;
+        }
+        public void setAddress(String address) {
+            this.address = address;
+        }
+    }
+}
